23bc452cb931767dfa58a9739b7ee3ab4f4d86c9
redblackblossom <physicsdev1029@gmail.com>
2025-05-31 13:21:30 +0900

    :sparkles: feat : add swagger setup (#8)

---
+++ src/main/java/com/depromeet/streetdrop/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.depromeet.streetdrop.global.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+				.group("v1")
+				.pathsToMatch("/**")
+				.build();
+	}
+
+	@Bean
+	public OpenAPI springOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Street Drop Server API")
+						.description("Street Drop Server API 명세서입니다.")
+						.version("v0.0.1"));
+	}
+}
