82376f312ebb614ee66e157686bce911a444882b
redblackblossom <physicsdev1029@gmail.com>
2025-05-31 17:30:59 +0900

    :sparkles: [feature] BaseTimeEntity, ResponseDto, PageResponseDto setting #16 (#17)

--- src/main/java/com/depromeet/streetdrop/global/common/presentation/HealthController.java
+++ src/main/java/com/depromeet/streetdrop/global/common/presentation/HealthController.java
@@ -1,4 +1,20 @@
 package com.depromeet.streetdrop.global.common.presentation;
 
+import com.depromeet.streetdrop.global.error.dto.ErrorCode;
+import com.depromeet.streetdrop.global.error.exception.common.NotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+	@GetMapping("/health")
+	public String health() {
+		return "Application Health Good!";
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		throw new NotFoundException(ErrorCode.NOT_FOUND);
+	}
 }
